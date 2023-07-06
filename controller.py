25c1e65dc6b615619128a1d030b92e7ae87010a8
bionca22 <bibicoder@gmail.com>
2023-07-06 09:22:39 -0300

    bagunça generalizada, porém, eu preciso mandar esses meus estudos iniciais aqui pra ajeitar meu ambiente

--- controller.py
+++ controller.py
@@ -1,19 +1,29 @@
 import urllib3
+from urllib import parse, request
+
 # import matplotlib.pyplot as plt
 # import numpy as np
 import json
 
+import pprint
+
 link = urllib3.PoolManager()
-ibge_api = "http://servicodados.ibge.gov.br/api/v3/agregados/8418/periodos/-6/variaveis/12747?localidades=N2[5]"
+ibge_api = "https/servicodados.ibge.gov.br/api/v3/agregados/1301/periodos/-6/variaveis/615|616?localidades=N2[all]"
 
+params = {"N2": "[all]"}
+querystring = parse.urlencode(params)
+url = "https://servicodados.ibge.gov.br/api/v3/agregados/1301/periodos/-6/variaveis/615|616?localidades=N2[all]" + "?" + querystring
+resp = request.urlopen(url)
+resp.isclosed()
+""" pprint.pprint(querystring) """
 
-response = link.request("GET", ibge_api)
+""" response = link.request("GET", ibge_api)
 content = response.data.decode('utf-8')
-data = json.loads(content)
-
-print("Response status:", response.status)
+data = json.loads(content) """
 
 # content_type = response.getheader("Content-Type")
 # print(content_type)
+
+""" print("Response status:", response.status)
 import pprint
-pprint.pprint(data[0]["resultados"][0]["series"][0]["localidade"]["nivel"]["nome"])
+pprint.pprint(data) """
