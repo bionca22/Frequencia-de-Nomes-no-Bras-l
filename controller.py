c547c368f9e24c78cb9eafb266284412d1e8d95b
bionca22 <bibicoder@gmail.com>
2023-07-21 21:40:52 -0300

    add os.system for better terminal view

--- controller.py
+++ controller.py
@@ -1,21 +1,32 @@
 import requests
 import json
 from matplotlib import pyplot as plt
+import os
 
-name = input("escreva um nome: ")
-response = json.loads(
-    requests.get("http://servicodados.ibge.gov.br/api/v2/censos/nomes/" + name).text
-)
 
-periodlist = []
-frequencylist = []
+def get_name(name: str) -> None:
+    if (name ==""):
+        return False
+    
+    response = json.loads(
+        requests.get("http://servicodados.ibge.gov.br/api/v2/censos/nomes/" + name).text
+    )
 
-for line in response[0]["res"]:
-    frequency = int(line["frequencia"])
-    period = line["periodo"].replace("[", " ")
 
-    periodlist.append(period)
-    frequencylist.append(frequency)
+    periodlist = []
+    frequencylist = []
 
-plt.plot(periodlist, frequencylist)
-plt.show()
+    for line in response[0]["res"]:
+        frequency = int(line["frequencia"])
+        period = line["periodo"].replace("[", " ")
+
+        periodlist.append(period)
+        frequencylist.append(frequency)
+
+    plt.plot(periodlist, frequencylist)
+    plt.show()
+    return True
+
+while(1):
+    os.system("cls")
+    get_name(input("Nome: "))
