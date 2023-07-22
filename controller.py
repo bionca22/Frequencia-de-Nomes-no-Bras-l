d136ea545404a76d8db0e1c849e76c855b7440ce
bionca22 <bibicoder@gmail.com>
2023-07-21 21:18:31 -0300

    atualização de código

--- controller.py
+++ controller.py
@@ -2,16 +2,18 @@ import requests
 import json
 from matplotlib import pyplot as plt
 
-
-response= json.loads(requests.get("http://servicodados.ibge.gov.br/api/v2/censos/nomes/" + nome).text)
+name = input("escreva um nome: ")
+response = json.loads(
+    requests.get("http://servicodados.ibge.gov.br/api/v2/censos/nomes/" + nome).text
+)
 
 periodlist = []
 frequencylist = []
 
-for line in response[0]['res']:
-    frequency = int(line['frequencia'])
-    period =line['periodo'].replace("[", " ")
-    
+for line in response[0]["res"]:
+    frequency = int(line["frequencia"])
+    period = line["periodo"].replace("[", " ")
+
     periodlist.append(period)
     frequencylist.append(frequency)
 
